2e3cb9fb8fcbaf9a22ee5d7790864b0218912940
Zied Snoussi <ziedsnoussi.tn@gmail.com>
2022-09-05 23:49:07 +0100

    Create Dashboard Screen

--- README.md
+++ README.md
@@ -156,3 +156,9 @@
     1. define protected route component
     2. define admin route component
     3. add menu for admin in header
+37. Create Dashboard Screen
+    1. create dashboard ui
+    2. implement backend api
+    3. connect ui to backend
+
+7:46
--- frontend/src/screens/DashboardScreen.js
+++ frontend/src/screens/DashboardScreen.js
@@ -1,7 +1,58 @@
-import React from 'react'
+import axios from 'axios';
+import React, { useContext, useEffect, useReducer } from 'react';
+import Row from 'react-bootstrap/Row';
+import LoadingBox from '../components/LoadingBox';
+import MessageBox from '../components/MessageBox';
+import { Store } from '../Store';
+import { getError } from '../utils';
 
+const reducer = (state, action) => {
+  switch (action.type) {
+    case 'FETCH_REQUEST':
+      return { ...state, loading: true };
+    case 'FETCH_SUCCESS':
+      return { ...state, summary: action.payload, loading: false };
+    case 'FETCH_FAIL':
+      return { ...state, loading: false, error: action.payload };
+    default:
+      return state;
+  }
+};
 export default function DashboardScreen() {
+  const [{ loading, summary, error }, dispatch] = useReducer(reducer, {
+    loading: true,
+    error: '',
+  });
+  const { state } = useContext(Store);
+  const { userInfo } = state;
+  useEffect(() => {
+    const fetchData = async () => {
+      try {
+        const { data } = await axios.get('/api/orders/summary', {
+          headers: { Authorization: `Bearer ${userInfo.token}` },
+        });
+        dispatch({ type: 'FETCH_SUCCESS', payload: data });
+      } catch (err) {
+        dispatch({
+          type: 'FETCH_FAIL',
+          payload: getError(err),
+        });
+      }
+    };
+    fetchData();
+  }, [userInfo]);
   return (
-    <div>DashboardScreen</div>
-  )
+    <div>
+      <h1>Dashboard</h1>
+      {loading ? (
+        <LoadingBox />
+      ) : error ? (
+        <MessageBox variant="danger">{error}</MessageBox>
+      ) : (
+        <>
+          <Row></Row>
+        </>
+      )}
+    </div>
+  );
 }
