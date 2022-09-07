2051f52c149fe962eea94e0bbe6b0eb9a35ae1ec
Zied Snoussi <ziedsnoussi.tn@gmail.com>
2022-09-07 18:29:36 +0100

    Create Dashboard Screen

--- frontend/src/screens/DashboardScreen.js
+++ frontend/src/screens/DashboardScreen.js
@@ -1,6 +1,9 @@
 import axios from 'axios';
 import React, { useContext, useEffect, useReducer } from 'react';
 import Row from 'react-bootstrap/Row';
+import Col from 'react-bootstrap/Col';
+import Card from 'react-bootstrap/Card';
+import Chart from 'react-google-charts';
 import LoadingBox from '../components/LoadingBox';
 import MessageBox from '../components/MessageBox';
 import { Store } from '../Store';
@@ -50,7 +53,79 @@ export default function DashboardScreen() {
         <MessageBox variant="danger">{error}</MessageBox>
       ) : (
         <>
-          <Row></Row>
+          <Row>
+            <Col md={4}>
+              <Card>
+                <Card.Body>
+                  <Card.Title>
+                    {summary.users && summary.users[0]
+                      ? summary.users[0].numUsers
+                      : 0}
+                  </Card.Title>
+                  <Card.Text>Users</Card.Text>
+                </Card.Body>
+              </Card>
+            </Col>
+            <Col md={4}>
+              <Card>
+                <Card.Body>
+                  <Card.Title>
+                    {summary.orders && summary.users[0]
+                      ? summary.orders[0].numOrders
+                      : 0}
+                  </Card.Title>
+                  <Card.Text>Orders</Card.Text>
+                </Card.Body>
+              </Card>
+            </Col>
+            <Col md={4}>
+              <Card>
+                <Card.Body>
+                  <Card.Title>
+                    $
+                    {summary.orders && summary.users[0]
+                      ? summary.orders[0].totalSales.toFixed(2)
+                      : 0}
+                  </Card.Title>
+                  <Card.Text>Orders</Card.Text>
+                </Card.Body>
+              </Card>
+            </Col>
+          </Row>
+          <div className="my-3">
+            <h2>Sales</h2>
+            {summary.dailyOrders.length === 0 ? (
+              <MessageBox>No Sale</MessageBox>
+            ) : (
+              <Chart
+                width="100%"
+                height="400px"
+                chartType="AreaChart"
+                loader={<div>Loading Chart...</div>}
+                data={[
+                  ['Date', 'Sales'],
+                  ...summary.dailyOrders.map((x) => [x._id, x.sales]),
+                ]}
+              ></Chart>
+            )}
+          </div>
+          <div className="my-3">
+            <h2>Categories</h2>
+            {summary.productCategories.length === 0 ? (
+              <MessageBox>No Category</MessageBox>
+            ) : (
+              <Chart
+                width="100%"
+                height="400px"
+                chartType="PieChart"
+                loader={<div>Loading Chart...</div>}
+                data={[
+                  ['Category', 'Products'],
+                  ...summary.productCategories.map((x) => [x._id, x.count]),
+                ]}
+              ></Chart>
+            )}
+          </div>
         </>
       )}
     </div>
