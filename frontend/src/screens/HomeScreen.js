b107b9f261371073a978acb4fcade7ce8cfe7099
Zied Snoussi <ziedsnoussi.tn@gmail.com>
2022-09-26 09:30:56 +0100

    change icon

--- frontend/src/screens/HomeScreen.js
+++ frontend/src/screens/HomeScreen.js
@@ -44,7 +44,7 @@ function HomeScreen() {
   return (
     <div>
       <Helmet>
-        <title>B2S</title>
+        <title>STAR SHOP</title>
       </Helmet>
       <h1>Featured Products</h1>
       <div className="products">
